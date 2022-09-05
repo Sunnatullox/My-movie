e700c59434ed968f93153be368d54a03afe18ab3
sunnatullox Hayitov <sunnatullox83@gmail.com>
2022-09-05 22:34:47 +0300

    Front-End created React App and Home Page

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.scss';
+import Home from './HomePage/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/HomePage/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AcUnit} from '@material-ui/icons'
+import './home.scss'
+
+function Home() {
+  return (
+    <div className='home'>
+        <AcUnit/>
+    </div>
+  )
+}
+
+export default Home
