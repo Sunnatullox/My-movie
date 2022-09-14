25c646680ff9a74179ef596a75b48520a2215f89
sunnatullox Hayitov <sunnatullox83@gmail.com>
2022-09-14 10:42:14 +0300

    make it better

--- api/index.js
+++ api/index.js
@@ -32,12 +32,7 @@ if(process.env.NODE_ENV === "production"){
   app.use(express.static("frontend/build"))
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname,"client", "build","index.html" ))
-  })
-  app.use(express.static( "adminPanel/build"))
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,"adminPanel", "build","index.html" ))
-  })
-  
+  })  
 }
 
 app.listen(PORT, () => {
